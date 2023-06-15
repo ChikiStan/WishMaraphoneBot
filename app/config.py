0e1299fbc6b246bb9722ac6c34330666e2643be3
ChikiStan <125257104+ChikiStan@users.noreply.github.com>
2023-06-15 15:59:33 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-TOKEN_API = "6112226257:AAEwsiAj5GohAAv9ITTg0MqcWuF0tKUAALk"
+TOKEN_API = "TG_BOT_API"
 bot = Bot(token=TOKEN_API)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
